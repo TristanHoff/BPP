8bd0cc278a9291070de21db87cba3249c9d9deac
TristanHoff <tristanhoff@hotmail.com>
2017-05-12 11:27:38 +0200

    BPPP
    NKLNASLNK

--- src/bpp/Product.java
+++ src/bpp/Product.java
@@ -3,39 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bpp;
+package bpp2;
+
+import java.util.Random;
 
 /**
  *
  * @author Tristan
  */
 public class Product {
-    private String naam;
-    private int grootte;
-    private String artikelNummer;
+    
+    private int grootte;    //Grootte van product
+    private int nummer;     //Artikelnummer
 
-    public Product(String naam, int grootte, String artikelNummer) {
-        this.naam = naam;
-        this.grootte = grootte;
-        this.artikelNummer = artikelNummer;
+    //Maakt random grootte van product
+    public Product(int n) {
+        Random rn = new Random();
+        this.grootte = rn.nextInt(5)+1;     
+        this.nummer = n;
     }
     
-
-    public String getNaam() {
-        return naam;
+    //Kan specifieke grootte meegeven aan product
+    public Product(int n, int g) {
+        this.nummer = n;
+        this.grootte = g;
     }
-
-    public int getGrootte() {
+    
+    public int getGrootte(){
         return grootte;
-    }
-
-    public String getArtikelNummer() {
-        return artikelNummer;
     }
 
     @Override
     public String toString() {
-        return  "ArtikelNummer= " + artikelNummer + ", Naam= " + naam + ", Grootte= " + grootte;
+        return "Product: " + nummer + "  Grootte: " + grootte;
     }
     
     
---
+++ src/bpp/Scherm.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bpp2;
+
+import java.awt.*;
+import static java.awt.BorderLayout.EAST;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author Tristan
+ */
+public class Scherm extends JFrame implements ActionListener{
+    
+    private JButton jbStart, jbStop, jbProduct, jbDoos;
+    private JLabel  jlAantalDozen, jlTijd, jlAlgoritme, jlVerdeling;
+    private JPanel  jp1, jp2, jp3, jp4, jp5, jp6;
+    
+    public Scherm() {
+        setTitle("Bin Packing Problem");
+        setSize(1360, 750);
+        setLayout(new BorderLayout());
+        
+        //Buttons
+        jbStart     = new JButton("Start proces");
+        jbStop      = new JButton("Stop proces");
+        jbProduct   = new JButton("Producten");
+        jbDoos      = new JButton("Doos");
+        
+        //Labels
+        jlAantalDozen = new JLabel("Aantal dozen: ");
+        jlTijd        = new JLabel("Tijd: ");
+        jlAlgoritme   = new JLabel("Algoritme: ");
+        jlVerdeling   = new JLabel("Optimale verdeling");
+        
+        
+        //Panels
+        JPanel jp = new JPanel(new BorderLayout());
+        JPanel jp1 = new JPanel(new BorderLayout());
+        JPanel jp2 = new JPanel(new FlowLayout());
+        JPanel jp3 = new JPanel(new FlowLayout());
+        JPanel jp4 = new JPanel(new FlowLayout());
+        JPanel jp5 = new JPanel(new FlowLayout());
+        
+        JPanel jp6 = new JPanel(new FlowLayout());
+        Box box = Box.createVerticalBox();
+        box.add(Box.createVerticalStrut(50));
+        box.add(jbStart);
+        box.add(Box.createVerticalStrut(50));
+        box.add(jbStop);
+        box.add(Box.createVerticalStrut(50));
+        box.add(jbProduct);
+        box.add(Box.createVerticalStrut(50));
+        box.add(jbDoos);
+        box.add(Box.createVerticalStrut(50));
+        jp6.add(box);
+        
+        jp1.setPreferredSize(new Dimension(400,695));
+       
+        add(jp6);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jbStart) {
+            //code voor Start knop
+        }
+        
+        if (e.getSource() == jbStop){
+            //code voor Stop knop
+        }
+        
+        if (e.getSource() == jbProduct){
+            //code voor Product knop
+        }
+        
+        if (e.getSource() == jbDoos){
+            //code voor Doos knop
+        }
+    }
+}
